9181abf358d24dfe292e3733dc65c3f62aa018e7
JohnLevander <jdl50@pitt.edu>
2016-01-19 16:03:23 -0500

    Added initial support for age ranges

--- socs_queries.py
+++ socs_queries.py
@@ -10,6 +10,7 @@ Description:
 Log:
     01/15/2016: Simple example to create dataframe queries given categorical variables.  Next step is to support
                 integer variables and age ranges.
+    01/19/2016: Started to implement age ranges.  Passing this off to Nick Millett for now.
 """
 
 
@@ -26,6 +27,29 @@ all_sick_by_sex["simulator_count_variables"] = {}
 all_sick_by_sex["simulator_count_variables"]['infection_state'] = ["LATENT", "INFECTIOUS"]
 all_sick_by_sex["simulator_count_variables"]['sex'] = ["M"]
 
+all_sick_by_sex_by_age_range = {}
+#this is the where clause
+all_sick_by_sex_by_age_range["simulator_count_variables"] = {}
+all_sick_by_sex_by_age_range["simulator_count_variables"]['infection_state'] = ["LATENT", "INFECTIOUS"]
+all_sick_by_sex_by_age_range["simulator_count_variables"]['sex'] = ["M", "F"]
+all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range'] = {}
+all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range']['driving_age'] = [16,999]
+all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range']['voting_age'] = [18,999]
+all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range']['retirement_age'] = [65,999]
+#this is the select clause
+all_sick_by_sex_by_age_range["output_options"] = {}
+all_sick_by_sex_by_age_range["output_options"]['axes'] = {'simulator_time', 'age_range'}
+
+all_sick_by_sex_by_age = {}
+#this is the where clause
+all_sick_by_sex_by_age["simulator_count_variables"] = {}
+all_sick_by_sex_by_age["simulator_count_variables"]['infection_state'] = ["LATENT", "INFECTIOUS"]
+all_sick_by_sex_by_age["simulator_count_variables"]['sex'] = ["M", "F"]
+all_sick_by_sex_by_age["simulator_count_variables"]['integer_age'] = {"min_age" : 30, "max_age" : 40}
+#this is the select clause
+all_sick_by_sex_by_age["output_options"] = {}
+all_sick_by_sex_by_age["output_options"]['axes'] = {'simulator_time', 'integer_age'}
+
 """
 TODO: Add support for these in next version.
 all_sick_by_sex_by_age = {}
@@ -34,11 +58,6 @@ all_sick_by_sex_by_age["simulator_count_variables"]['infection_states'] = ["LATE
 all_sick_by_sex_by_age["simulator_count_variables"]['sex'] = ["M", "F"]
 all_sick_by_sex_by_age["simulator_count_variables"]['integer_age'] = True
 
-all_sick_by_sex_by_age_range = {}
-all_sick_by_sex_by_age_range["simulator_count_variables"] = {}
-all_sick_by_sex_by_age_range["simulator_count_variables"]['infection_states'] = ["LATENT", "INFECTIOUS"]
-all_sick_by_sex_by_age_range["simulator_count_variables"]['sex'] = ["M", "F"]
-all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range'] = [0, 5, 18, 25, 65, 100]
 
 
 all_sick_by_sex_by_age_state = {}
@@ -50,11 +69,39 @@ all_sick_by_sex_by_age_state["simulator_count_variables"]['location_admin1'] = "
 """
 
 
+def add_age_range_column(scos, df):
+
+    def filter_out_unrequested_age_ranges(df, age_range):
+    #build the query
+    #remove everything below minnist min, and above maxxest max
+        query = "(integer_age >= "+age_range[0] +") & (integer_age <= 25)"
+        df = df.query(query)
+        return df
+
+
+    def make_age_range_row(row):
+        return pd.cut([row['integer_age']], bins=bins, labels=bin_labels)[0]
+
+    df = filter_out_unrequested_age_ranges(df)
+    #nick create the bins given all_sick_by_sex_by_age_range["simulator_count_variables"]['age_range']
+    #here is some help:
+    age_ranges = scos["simulator_count_variables"]['age_range']
+    for age_range in age_ranges:
+        print (age_range + " bin is " + str(age_ranges[age_range][0]) + " to " + str(age_ranges[age_range][1]))
+
+    #result of nicks awesome code is bins and labels
+    bins = [0,2,65,100]
+    bin_labels = ['infant', 'unwanted_1', 'retirement']
+
+    df['age_range'] = df.apply(make_age_range_row, axis=1)
+
+    return df
+
 """
-This function creates queries in the form of: df.query('b == ["a", "b", "c"]')
+This function creates queries in the form of: 'b == ["a", "b", "c"]'
 The query selects all rows in the dataframe where column b is equal to the value a b or c.
 """
-def create_query(df, col_name, vals_to_keep):
+def create_category_query(df, col_name, vals_to_keep):
     query = df.columns[df.columns.get_loc(col_name)] + ' == ['
     for val in vals_to_keep:
         query += "'" + val + "',"
@@ -62,25 +109,34 @@ def create_query(df, col_name, vals_to_keep):
     query += "]"
     return query
 
-
 """
-Here we SELECT only rows that we want based on the simulator_count_variables.  For example if the user
+Here we enforce the WHERE clause.  We filter out rows that we do not want, want based on the simulator_count_variables.
+For example if the user
 only wanted to see the rows that contain data for MALES, it would be specified in the simulator_count_variables, and we
 would filter the FEMALES out in this function.
 """
 def filter_df(df, scos):
     #TODO: deal with age_range categories and integer categories
     for simulator_count_variable in scos["simulator_count_variables"]:
-        query = create_query(df, simulator_count_variable, scos['simulator_count_variables'][simulator_count_variable])
-        df = df.query(query)
+        if simulator_count_variable != "age_range":
+            query = create_category_query(df, simulator_count_variable, scos['simulator_count_variables'][simulator_count_variable])
+            df = df.query(query)
     return df
 
+
+
+
 if __name__ == '__main__':
+    scos = all_sick_by_sex_by_age_range
+
     #load the simulator output into a dataframe
     line_listing = pd.read_csv('http://research.rods.pitt.edu/line_listing.csv')
 
+
     #Filter out rows from the dataframe that we don't want
-    df = filter_df(line_listing, all_sick_by_sex)
+    df = filter_df(line_listing, scos)
+
+    df = add_age_range_column(scos, df)
 
     #TODO: Add aggregation options next, once data is filtered
     print (df)
