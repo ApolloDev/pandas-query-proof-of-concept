4fbc5b77435afe9fdffcc5e3ce1b72ba180324c6
Millett <nem41@nem41-mnb.local>
2016-05-11 11:56:23 -0400

    fixes

--- parse_example_scos_message.py
+++ parse_example_scos_message.py
@@ -1,5 +1,4 @@
 import xml.etree.ElementTree as ET
-import math
 
 APOLLO_TYPES_NAMESPACE = 'http://types.apollo.pitt.edu/v4_0/'
 XSI_TYPE = '{http://www.w3.org/2001/XMLSchema-instance}type'
@@ -32,8 +31,6 @@ def process_other_variables(query, base):
             query["simulator_count_variables"][range_type][bound_text]['range_units'] = unit_of_measure.text
             query["simulator_count_variables"][range_type][bound_text]['range'] = [query_lb, query_ub]
 
-
-
 def process_boundary(boundary_element):
     finite_boundry = boundary_element.find('{' + APOLLO_TYPES_NAMESPACE + '}finiteBoundary')
     infinite_boundry = boundary_element.find('{' + APOLLO_TYPES_NAMESPACE + '}infiniteBoundary')
@@ -47,12 +44,42 @@ def process_boundary(boundary_element):
             lb = float("inf")
     return lb
 
-if __name__ == '__main__':
+def process_spatial_granularity(query, element):
+    if element.text != 'none':
+        if element.text == 'admin0':
+            query["output_options"]['axes'].append('location_admin0')
+        elif element.text == 'admin1':
+            query["output_options"]['axes'].append('location_admin0')
+            query["output_options"]['axes'].append('location_admin1')
+        elif element.text == 'admin2':
+            query["output_options"]['axes'].append('location_admin0')
+            query["output_options"]['axes'].append('location_admin1')
+            query["output_options"]['axes'].append('location_admin2')
+        elif element.text == 'admin3':
+            query["output_options"]['axes'].append('location_admin0')
+            query["output_options"]['axes'].append('location_admin1')
+            query["output_options"]['axes'].append('location_admin2')
+            query["output_options"]['axes'].append('location_admin3')
+        elif element.text == 'admin4':
+            query["output_options"]['axes'].append('location_admin0')
+            query["output_options"]['axes'].append('location_admin1')
+            query["output_options"]['axes'].append('location_admin2')
+            query["output_options"]['axes'].append('location_admin3')
+            query["output_options"]['axes'].append('location_admin4')
+        elif element.text == 'admin5':
+            query["output_options"]['axes'].append('location_admin0')
+            query["output_options"]['axes'].append('location_admin1')
+            query["output_options"]['axes'].append('location_admin2')
+            query["output_options"]['axes'].append('location_admin3')
+            query["output_options"]['axes'].append('location_admin4')
+            query["output_options"]['axes'].append('location_admin5')
+        elif element.text == 'latLong':
+            print ("Error: latLong coordinates are not currently supported")
 
-    tree = ET.parse('/Users/nem41/Documents/code_projects/apollo_projects/example-scos-messages/S_E_I_R_new_I_by_household_income.xml')
-    root = tree.getroot()
+def get_queries_from_scos(scos_xml_root_node):
 
-    for count_specification in root.findall('{' + APOLLO_TYPES_NAMESPACE + '}SimulatorCountOutputSpecification'):
+    queries = []
+    for count_specification in scos_xml_root_node.findall('{' + APOLLO_TYPES_NAMESPACE + '}SimulatorCountOutputSpecification'):
 
         query = {}
         query["simulator_count_variables"] = {}
@@ -68,7 +95,8 @@ if __name__ == '__main__':
                 namespace = (element.tag.split('}', 1)[0])[1:]  # strip all namespaces
 
             if namespace != APOLLO_TYPES_NAMESPACE:
-                print "Error: unsupported Apollo type namespace was used in the XML"
+                print("Error: unsupported Apollo type namespace was used in the XML")
+                return None
 
             if field == 'speciesToCount':
                 query["simulator_count_variables"]['species'] = {element.text}
@@ -76,58 +104,33 @@ if __name__ == '__main__':
             elif field == 'temporalGranularity':
                 if element.text != 'entireSimulation':
                     if element.text != 'eachSimulationTimestep':
-                        # need to do further aggregation
-                        print 'Error: currently the only supported temporalGranularity is eachSimulationTimestep'
+                        print('Error: unrecognized temporalGranularity. The available options are \'entireSimulation\' and \'eachSimulationTimestep\'')
+                        return None
                     query["output_options"]['axes'].append('simulator_time')
             elif field == 'spatialGranularity':
-                if element.text != 'none':
-                    if element.text == 'admin0':
-                        query["simulator_count_variables"]['location_admin0'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                    elif element.text == 'admin1':
-                        query["simulator_count_variables"]['location_admin1'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                        query["output_options"]['axes'].append('location_admin1')
-                    elif element.text == 'admin2':
-                        query["simulator_count_variables"]['location_admin2'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                        query["output_options"]['axes'].append('location_admin1')
-                        query["output_options"]['axes'].append('location_admin2')
-                    elif element.text == 'admin3':
-                        query["simulator_count_variables"]['location_admin3'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                        query["output_options"]['axes'].append('location_admin1')
-                        query["output_options"]['axes'].append('location_admin2')
-                        query["output_options"]['axes'].append('location_admin3')
-                    elif element.text == 'admin4':
-                        query["simulator_count_variables"]['location_admin4'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                        query["output_options"]['axes'].append('location_admin1')
-                        query["output_options"]['axes'].append('location_admin2')
-                        query["output_options"]['axes'].append('location_admin3')
-                        query["output_options"]['axes'].append('location_admin4')
-                    elif element.text == 'admin5':
-                        query["simulator_count_variables"]['location_admin5'] = True
-                        query["output_options"]['axes'].append('location_admin0')
-                        query["output_options"]['axes'].append('location_admin1')
-                        query["output_options"]['axes'].append('location_admin2')
-                        query["output_options"]['axes'].append('location_admin3')
-                        query["output_options"]['axes'].append('location_admin4')
-                        query["output_options"]['axes'].append('location_admin5')
-                    elif element.text == 'latLong':
-                        print "Error: latLong coordinates are not currently supported"
+                process_spatial_granularity(query, element)
             elif field == 'infectionState':
                 if 'infection_state' in query["simulator_count_variables"]:
-                 query["simulator_count_variables"]['infection_state'].add(element.text)
+                 query["simulator_count_variables"]['infection_state'].add(element.text.upper())
                 else:
-                    query["simulator_count_variables"]['infection_state'] = {element.text}
+                    query["simulator_count_variables"]['infection_state'] = {element.text.upper()}
+                    query["output_options"]['axes'].append('infection_state')
             elif field == 'diseaseOutcome':
                 if 'disease_state' in query["simulator_count_variables"]:
-                 query["simulator_count_variables"]['disease_state'].add(element.text)
+                 query["simulator_count_variables"]['disease_state'].add(element.text.upper())
                 else:
-                    query["simulator_count_variables"]['disease_state'] = {element.text}
+                    query["simulator_count_variables"]['disease_state'] = {element.text.upper()}
+                    query["output_options"]['axes'].append('disease_state')
             elif field == 'otherVariables':
                 process_other_variables(query, element)
 
 
-        print query
+        queries.append(query)
+    return queries
+
+if __name__ == '__main__':
+
+    tree = ET.parse('/Users/nem41/Documents/code_projects/apollo_projects/example-scos-messages/num_infected_by_location.xml')
+    root = tree.getroot()
+    queries = get_queries_from_scos(root)
+    print(queries)
--- socs_queries.py
+++ socs_queries.py
@@ -1,4 +1,6 @@
+import xml.etree.ElementTree as ET
 import pandas as pd
+from parse_example_scos_message import get_queries_from_scos
 
 """
 Author: John Levander
@@ -68,38 +70,68 @@ all_sick_by_sex_by_age_state["simulator_count_variables"]['integer_age'] = "all"
 all_sick_by_sex_by_age_state["simulator_count_variables"]['location_admin1'] = "all"
 """
 
+m_inf = -float('inf')
+p_inf = float('inf')
 
-def filter_age_ranges(scos, df):
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
-    def filter_age_ranges(df, min, max):
+def filter_ranges(scos, df):
+
+    column_name = ''
+    if 'age_range' in scos["simulator_count_variables"]:
+        variable = 'age_range'
+        column_name = 'integer_age'
+    elif 'household_median_income' in scos["simulator_count_variables"]:
+        variable = 'household_median_income'
+        column_name = 'household_median_income'
+    else:
+        return df;
+
+    def filter_ranges_min_max(df, min, max):
+
+        if min == float('-inf') and max == float('inf'):
+            # no filtering required
+            return df
+
+        if min == float('-inf'):
+            query = "(" + column_name + " <= " + str(max) + ")"
+        elif max == float('inf'):
+            query = "(" + column_name + " >= " + str(min) + ")"
+        else:
+            query = "(" + column_name + " >= " + str(min) + ") & (" + column_name + " <= " + str(max) + ")"
+
         # remove rows with age below min or above max
-        query = "(integer_age >= " + str(min) + ") & (integer_age <= " + str(max) + ")"
         df = df.query(query)
         return df
 
-    age_ranges = scos["simulator_count_variables"]['age_range']
+    ranges = scos["simulator_count_variables"][variable]
 
 
     # get min and max from all ranges
-    min = 200
-    max = 0
-    for age_range in age_ranges:
-        if age_ranges[age_range][0] < min:
-            min = age_ranges[age_range][0]
+    min = float("inf")
+    max = -float("inf")
+    for age_range in ranges:
+        if ranges[age_range]['range'][0] < min:
+            min = ranges[age_range]['range'][0]
 
-        if age_ranges[age_range][1] > max:
-            max = age_ranges[age_range][1]
+        if ranges[age_range]['range'][1] > max:
+            max = ranges[age_range]['range'][1]
 
 
     # filter out all ages outside min and max
-    df = filter_age_ranges(df, min, max)
+    df = filter_ranges_min_max(df, min, max)
 
     count = 0
-    for age_range in age_ranges:
-        print (age_range + " bin is " + str(age_ranges[age_range][0]) + " to " + str(age_ranges[age_range][1]))
+    for age_range in ranges:
+        print (age_range + " bin is " + str(ranges[age_range]['range'][0]) + " to " + str(ranges[age_range]['range'][1]))
         # get copy of dataframe to filter
         dfcopy = df.copy()
-        dfcopy = filter_age_ranges(dfcopy, age_ranges[age_range][0], age_ranges[age_range][1])
+        dfcopy = filter_ranges_min_max(dfcopy, ranges[age_range]['range'][0], ranges[age_range]['range'][1])
         # add the age range column to the data frame
         dfcopy['age_range'] = age_range
         if count == 0:
@@ -117,7 +149,11 @@ The query selects all rows in the dataframe where column b is equal to the value
 def create_category_query(df, col_name, vals_to_keep):
     query = df.columns[df.columns.get_loc(col_name)] + ' == ['
     for val in vals_to_keep:
-        query += "'" + val + "',"
+        # see if val is an integer
+        if is_number(val):
+            query += val + ","
+        else:
+            query += "'" + val + "',"
     query = query[:-1]
     query += "]"
     return query
@@ -131,7 +167,7 @@ would filter the FEMALES out in this function.
 def filter_df(df, scos):
     #TODO: deal with age_range categories and integer categories
     for simulator_count_variable in scos["simulator_count_variables"]:
-        if simulator_count_variable != "age_range":
+        if simulator_count_variable != "age_range" and simulator_count_variable != "household_median_income":
             query = create_category_query(df, simulator_count_variable, scos['simulator_count_variables'][simulator_count_variable])
             df = df.query(query)
     return df
@@ -146,18 +182,28 @@ def process_output_options(df, socs):
 
 
 if __name__ == '__main__':
-    scos = all_sick_by_sex_by_age_range
+
+    tree = ET.parse('/Users/nem41/Documents/code_projects/apollo_projects/example-scos-messages/new_E_total_vacc_by_household_income.xml')
+    root = tree.getroot()
+    queries = get_queries_from_scos(root)
+
+    #scos = all_sick_by_sex_by_age_range
+
+    scos = queries[0]
 
     #load the simulator output into a dataframe
-    line_listing = pd.read_csv('http://research.rods.pitt.edu/line_listing.csv')
+    line_listing = pd.read_csv('http://research.rods.pitt.edu/line_listing_new.csv')
 
+    # line_listing['species'] = line_listing['species'].astype(str)
 
     #Filter out rows from the dataframe that we don't want
     df = filter_df(line_listing, scos)
 
-    df = filter_age_ranges(scos, df)
+    df = filter_ranges(scos, df)
 
     df = process_output_options(df, scos)
+
+    df.to_csv('/Users/nem41/Documents/apollo/python_output_test.csv', sep=',', index=False)
 
     #TODO: Add aggregation options next, once data is filtered
     print (df)
